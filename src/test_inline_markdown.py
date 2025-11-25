64235106d1f02d3755081e3b7921d90d410092d9
Fives555555 <108680435+Fives555555@users.noreply.github.com>
2025-11-25 22:08:06 +0000

    Convert text to TextNode objects

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link
+from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_image, split_nodes_link, text_to_textnodes
 from textnode import TextNode, TextType
 
 class TestInlineMarkdown(unittest.TestCase):
@@ -89,6 +89,10 @@ class TestInlineMarkdown(unittest.TestCase):
         node = TextNode("This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows", TextType.TEXT)
         new_nodes = split_nodes_link([node])
         self.assertListEqual([TextNode("This is text with a ", TextType.TEXT), TextNode("link", TextType.LINK, "https://boot.dev"), TextNode(" and ", TextType.TEXT), TextNode("another link", TextType.LINK, "https://blog.boot.dev"), TextNode(" with text that follows", TextType.TEXT)], new_nodes)
+
+    def test_text_to_textnodes(self):
+        nodes = text_to_textnodes("This is **bold text** with an _italic_ word, a `code block`, an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)")
+        self.assertListEqual(nodes, [TextNode("This is ", TextType.TEXT), TextNode("bold text", TextType.BOLD), TextNode(" with an ", TextType.TEXT), TextNode("italic", TextType.ITALIC), TextNode(" word, a ", TextType.TEXT), TextNode("code block", TextType.CODE), TextNode(", an ", TextType.TEXT), TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"), TextNode(" and a ", TextType.TEXT), TextNode("link", TextType.LINK, "https://boot.dev")])
 
 if __name__ == "__main__":
     unittest.main()
